b22554210a147cd4b094d30bd2c2c2226c805d05
Muridi Jeilani <Muridi12@hotmail.com>
2025-02-02 20:08:23 -0500

    Homework1

---
+++ Homework1.py
@@ -0,0 +1,8 @@
+length = 10
+width = 7
+perimter = 2*(length+width)
+area = length*width
+print("The Floor material required is:") 
+print(area)
+print("The baseboard material required is:") 
+print(perimter)
